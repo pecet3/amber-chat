3222329e12c7df802a369b1cf24a8306d70818e6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-11 17:49:44 +0200

    created the component

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,7 +1,60 @@
-export const Chat: React.FC = () => {
+import { useState, useEffect } from "react";
+import {
+  addDoc,
+  collection,
+  serverTimestamp,
+  onSnapshot,
+  query,
+  where,
+} from "firebase/firestore";
+import { auth, db } from "../firebase-config";
+
+export interface IChat {
+  room: string;
+}
+export const Chat: React.FC<IChat> = ({ room }) => {
+  const [newMessage, setNewMessage] = useState("");
+
+  const messagesRef = collection(db, "messages");
+
+  useEffect(() => {
+    const queryMessages = query(messagesRef, where("room", "==", room));
+    onSnapshot(queryMessages, (snapshot) => {
+      console.log("newMessage");
+    });
+  });
+
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    if (newMessage === "") return;
+
+    await addDoc(messagesRef, {
+      text: newMessage,
+      createdAt: serverTimestamp(),
+      user: auth.currentUser ? auth.currentUser.displayName : "",
+      room,
+    });
+
+    setNewMessage;
+  };
+  const handleInputChange = (
+    event: React.FormEvent<HTMLInputElement>
+  ): void => {
+    setNewMessage(event.currentTarget.value);
+  };
   return (
-    <div>
-      <h1>HEJ</h1>
+    <div className="">
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Type your message..."
+          onChange={handleInputChange}
+          value={newMessage}
+          className="rounded-md border-2 p-2"
+        />
+        <button type="submit" className="submitButton">
+          Send
+        </button>
+      </form>
     </div>
   );
 };
