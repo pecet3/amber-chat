24389b442d055b767713e5957ab5821f149bd4c6
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-11 20:20:17 +0200

    edited functions, updated tailwind

--- src/components/Chat.tsx
+++ src/components/Chat.tsx
@@ -1,4 +1,5 @@
 import { useState, useEffect, useRef } from "react";
+import { nanoid } from "nanoid";
 import {
   addDoc,
   collection,
@@ -14,20 +15,28 @@ export interface IChat {
 }
 export const Chat: React.FC<IChat> = ({ room }) => {
   const [newMessage, setNewMessage] = useState("");
-  const [messages, setMessages] = useState([]);
+  const [messages, setMessages] = useState([
+    {
+      text: "",
+      user: "",
+      createdAt: "",
+    },
+  ]);
   const buttonRef = useRef<HTMLInputElement>(null);
 
   const messagesRef = collection(db, "messages");
 
   useEffect(() => {
     const queryMessages = query(messagesRef, where("room", "==", room));
-    onSnapshot(queryMessages, (snapshot) => {
-      let messages = [];
+    const unSubscribe = onSnapshot(queryMessages, (snapshot) => {
+      let messages: Array<any> = [];
       snapshot.forEach((doc) => {
         messages.push({ ...doc.data(), id: doc.id });
       });
+      setMessages(messages);
     });
-    setMessages(messages);
+
+    return () => unSubscribe();
   }, []);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
@@ -50,19 +59,36 @@ export const Chat: React.FC<IChat> = ({ room }) => {
     setNewMessage(event.currentTarget.value);
   };
   return (
-    <div className="">
-      <form onSubmit={handleSubmit}>
-        <input
-          placeholder="Type your message..."
-          onChange={handleInputChange}
-          value={newMessage}
-          ref={buttonRef}
-          className="rounded-md border-2 p-2"
-        />
-        <button type="submit" name="submitButton" className="submitButton">
-          Send
-        </button>
-      </form>
-    </div>
+    <>
+      <div className="flex flex-col items-center">
+        {messages.map((message) => (
+          <div
+            key={nanoid()}
+            className="my-2 flex w-[460px] items-center justify-between gap-8 rounded-md border-2 bg-slate-50 p-2 md:w-[660px]"
+          >
+            <p className="w-32 justify-items-start font-serif text-slate-700">
+              {message.user !== "" && message.user}:
+            </p>
+            <p className="m-0 w-96 grow break-all px-8 text-right text-lg">
+              {message.text !== "" && message.text}
+            </p>
+          </div>
+        ))}
+      </div>
+      <div className="">
+        <form onSubmit={handleSubmit}>
+          <input
+            placeholder="Type your message..."
+            onChange={handleInputChange}
+            value={newMessage}
+            ref={buttonRef}
+            className="rounded-md border-2 p-2 "
+          />
+          <button type="submit" name="submitButton" className="submitButton">
+            Send
+          </button>
+        </form>
+      </div>
+    </>
   );
 };
