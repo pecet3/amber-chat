3ce8a8963a8b71c76ac4cf520802d3c3783a61ad
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-11 15:43:38 +0200

    edited app

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,34 @@
 import { useState } from "react";
-
+import { Auth } from "./components/Auth";
 import "./App.css";
+import Cookies from "universal-cookie";
+const cookies = new Cookies();
 
+type TAuth = Cookies;
 function App() {
-  return <div className="App">hi</div>;
+  const [isAuth, setIsAuth] = useState<TAuth>(cookies.get("auth-token"));
+  const [room, setRoom] = useState("");
+
+  if (!isAuth) {
+    return (
+      <div className="App">
+        <Auth />
+      </div>
+    );
+  }
+  return (
+    <div>
+      {room ? (
+        <div>Chat</div>
+      ) : (
+        <div>
+          <label>Enter Room Name</label>
+          <input></input>
+          <button>EnterChat</button>
+        </div>
+      )}
+    </div>
+  );
 }
 
 export default App;
