488f144487c991a438f4c3352fc510f2aa3d179b
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-16 20:40:28 +0200

    implemented Header

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -1,18 +1,10 @@
 import React from "react";
-import { auth, provider } from "../../firebase-config";
-import {
-  signInAnonymously,
-  onAuthStateChanged,
-  createUserWithEmailAndPassword,
-  signInWithEmailAndPassword,
-  signOut,
-  User,
-} from "firebase/auth";
-import Cookies from "universal-cookie";
+import { auth } from "../../firebase-config";
+import { onAuthStateChanged, signOut, User } from "firebase/auth";
 import { GoogleAuth } from "./GoogleAuth";
 import { LogIn } from "./LogIn";
 import { Register } from "./Register";
-const cookies = new Cookies();
+import { Header } from "../../common/Header";
 
 export interface IAuth {
   setAuthTrue: React.Dispatch<React.SetStateAction<Boolean>>;
@@ -40,13 +32,11 @@ export const Auth: React.FC<IAuth> = ({ setAuthTrue }) => {
     return () => unSubscribe();
   }, []);
   return (
-    <div className="my-20">
-      <p>Welcome to...</p>
-      <h1 className="mb-10 text-4xl">JakubChat</h1>
-      <h1 className="text-4xl">{user && user.email}</h1>
+    <>
+      <Header />
       <Register setAuthTrue={setAuthTrue} user={user} setUser={setUser} />
       <LogIn setAuthTrue={setAuthTrue} user={user} setUser={setUser} />
       <GoogleAuth setAuthTrue={setAuthTrue} />
-    </div>
+    </>
   );
 };
