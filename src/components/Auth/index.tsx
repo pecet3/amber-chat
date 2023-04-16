e612ea19d0153978f3b81eefc769708a131fe20f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-16 20:26:40 +0200

    cut component

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -10,6 +10,8 @@ import {
 } from "firebase/auth";
 import Cookies from "universal-cookie";
 import { GoogleAuth } from "./GoogleAuth";
+import { LogIn } from "./LogIn";
+import { Register } from "./Register";
 const cookies = new Cookies();
 
 export interface IAuth {
@@ -20,79 +22,11 @@ export type TLoginData = {
   password: string;
 };
 export const Auth: React.FC<IAuth> = ({ setAuthTrue }) => {
-  const [loginInput, setLoginInput] = React.useState({
-    email: "",
-    password: "",
-  });
-  const [registerInput, setRegisterInput] = React.useState<TLoginData>({
-    email: "",
-    password: "",
-  });
-
   const [user, setUser] = React.useState<User | null>(null);
-
-  const registerEmailOnChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    setRegisterInput({
-      ...registerInput,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const registerPasswordOnChange = (
-    e: React.ChangeEvent<HTMLInputElement>
-  ): void => {
-    setRegisterInput({
-      ...registerInput,
-      [e.target.name]: e.target.value,
-    });
-  };
-  const loginEmailOnChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    setLoginInput({
-      ...loginInput,
-      [e.target.name]: e.target.value,
-    });
-  };
-
-  const loginPasswordOnChange = (
-    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
-  ): void => {
-    setLoginInput({
-      ...loginInput,
-      [e.target.name]: e.target.value,
-    });
-  };
 
   React.useEffect(() => {
     console.log(user);
   }, [user]);
-
-  const register = async () => {
-    try {
-      const user = await createUserWithEmailAndPassword(
-        auth,
-        registerInput.email,
-        registerInput.password
-      );
-      console.log(user);
-    } catch (err) {
-      console.error(err);
-    }
-  };
-  const logIn = async () => {
-    try {
-      const user = await signInWithEmailAndPassword(
-        auth,
-        loginInput.email,
-        loginInput.password
-      );
-      console.log(user);
-    } catch (err) {
-      console.error(err);
-    }
-  };
 
   const logOut = async () => {
     await signOut(auth);
@@ -109,69 +43,10 @@ export const Auth: React.FC<IAuth> = ({ setAuthTrue }) => {
     <div className="my-20">
       <p>Welcome to...</p>
       <h1 className="mb-10 text-4xl">JakubChat</h1>
-      <div>
-        <form
-          className="m-auto flex max-w-[240px] flex-col 
-        items-center gap-3 rounded-lg border-2
-         border-blue-400 bg-slate-300 p-2 py-4"
-          onSubmit={(e) => {
-            e.preventDefault();
-            register();
-          }}
-        >
-          <legend>Don't have an account? Register here!</legend>
-          <input
-            type="text"
-            className="max-w-[162px] rounded-md p-2 text-center shadow-md"
-            name="email"
-            value={registerInput.email}
-            placeholder="Enter your email"
-            onChange={registerEmailOnChange}
-          />
-          <input
-            type="password"
-            name="password"
-            className="max-w-[162px] rounded-md p-2 text-center shadow-md"
-            value={registerInput.password}
-            placeholder="Your password"
-            onChange={registerPasswordOnChange}
-          />
-          <button className="submitButton px-6">Register</button>
-        </form>
-        <h1 className="text-4xl">{user && user.email}</h1>
-        <form
-          className="m-auto flex max-w-[240px] flex-col 
-          items-center gap-3 rounded-lg border-2
-         border-blue-400 bg-slate-300 p-2 py-4"
-          onSubmit={(e) => {
-            e.preventDefault();
-            logIn();
-          }}
-        >
-          <legend>Do you have an account? Log in, now!</legend>
-          <input
-            type="text"
-            className="max-w-[162px] rounded-md p-2 text-center shadow-md"
-            value={loginInput.email}
-            placeholder="Enter your name..."
-            name="email"
-            onChange={loginPasswordOnChange}
-          />
-          <input
-            type="password"
-            className="max-w-[162px] rounded-md p-2 text-center shadow-md"
-            value={loginInput.password}
-            name="password"
-            placeholder="your password"
-            onChange={loginPasswordOnChange}
-          />
-
-          <button className="submitButton px-6" onClick={() => console.log()}>
-            Log In
-          </button>
-        </form>
-        <GoogleAuth setAuthTrue={setAuthTrue} />
-      </div>
+      <h1 className="text-4xl">{user && user.email}</h1>
+      <Register setAuthTrue={setAuthTrue} user={user} setUser={setUser} />
+      <LogIn setAuthTrue={setAuthTrue} user={user} setUser={setUser} />
+      <GoogleAuth setAuthTrue={setAuthTrue} />
     </div>
   );
 };
