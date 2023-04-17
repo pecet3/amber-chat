0b4a9e5f877fba1956a3d282090799218000f1bc
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-17 13:34:15 +0200

    form is done

--- src/components/EditProfile.tsx
+++ src/components/EditProfile.tsx
@@ -2,27 +2,43 @@ import React from "react";
 import { updateProfile, User } from "firebase/auth";
 import { auth } from "../firebase-config";
 export const EditProfile = () => {
-  const [name, setName] = React.useState({
+  const [user, setUser] = React.useState({
     name: "",
   });
-  const updateDisplayName = async () => {
+  const updateData = async () => {
     try {
       updateProfile(auth.currentUser as User, {
-        displayName: "test1234",
+        displayName: !!user.name ? user.name : auth.currentUser?.displayName,
       });
     } catch (err) {
       alert(err);
     }
   };
+  const onChange = (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => {
+    setUser({ ...user, [e.target.name]: e.target.value });
+    console.log(user.name);
+  };
+
   return (
-    <section className="flex">
-      <form>
+    <section>
+      <form
+        className="form"
+        onSubmit={(e) => {
+          e.preventDefault();
+          updateData();
+        }}
+      >
         <input
           type="text"
           className="inputElement"
           name="name"
-          value={name.name}
+          onChange={onChange}
+          value={user.name}
+          placeholder="Enter new name..."
         />
+        <button className="submitButton">Update your profile!</button>
       </form>
     </section>
   );
