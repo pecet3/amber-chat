a789fbca4d667e9fb45e44f16f646ebd8ed30de5
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-17 22:13:09 +0200

    motion in component

--- src/common/Header.tsx
+++ src/common/Header.tsx
@@ -1,8 +1,18 @@
+import React from "react";
+import { motion } from "framer-motion";
 export const Header = () => {
   return (
     <header className="my-10">
       <p>Welcome to...</p>
-      <h1 className="text-4xl">JakubChat</h1>
+
+      <motion.h1
+        className="text-4xl"
+        animate={{ scale: 1 }}
+        initial={{ scale: 0 }}
+        transition={{ duration: 0.5 }}
+      >
+        JakubChat
+      </motion.h1>
     </header>
   );
 };
