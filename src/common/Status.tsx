61b20e608a8282a5d2ca8ea4c39cb78130e5bc94
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-18 00:05:27 +0200

    removed component

--- src/common/Status.tsx
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import Context, { TContext } from "../ChatContext";
-export const Status = () => {
-  const { status } = React.useContext(Context) as TContext;
-  return <p className="m-2 text-sm">*{status && status.message}</p>;
-};
