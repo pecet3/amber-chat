b0a2f7881e70ab0c3eab86323a2330ed72b745ec
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-17 13:11:14 +0200

    implemented setRoom

--- src/common/LogOutButton.tsx
+++ src/common/LogOutButton.tsx
@@ -3,13 +3,15 @@ import { auth } from "../firebase-config";
 import { signOut } from "firebase/auth";
 import Context, { TContext } from "../ChatContext";
 import Cookies from "universal-cookie";
+
 const cookies = new Cookies();
 export const LogOutButton: React.FC = () => {
-  const { setIsAuth } = React.useContext(Context) as TContext;
+  const { setIsAuth, setRoom } = React.useContext(Context) as TContext;
   const logOut = async () => {
     try {
       await signOut(auth);
       setIsAuth(false);
+      setRoom("");
       cookies.remove("auth-token");
     } catch (err) {
       alert(err);
