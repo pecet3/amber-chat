c92ba91ed70e6b8a7ca15625133387082515bbc0
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-18 00:06:19 +0200

    implemented status

--- src/components/Auth/Anonymous.tsx
+++ src/components/Auth/Anonymous.tsx
@@ -7,7 +7,7 @@ import Context, { TContext } from "../../ChatContext";
 const cookies = new Cookies();
 export const Anonymous: React.FC = () => {
   const [nameInput, setNameInput] = React.useState("");
-  const { setIsAuth } = React.useContext(Context) as TContext;
+  const { setIsAuth, setStatus } = React.useContext(Context) as TContext;
 
   const getIn = async () => {
     try {
@@ -23,26 +23,35 @@ export const Anonymous: React.FC = () => {
   };
 
   return (
-    <form
-      className="flex justify-center gap-2"
-      onSubmit={(e) => {
-        e.preventDefault();
-        if (nameInput.length < 3) return;
-        getIn();
-      }}
-    >
-      <input
-        type="text"
-        name="name"
-        className="inputElement"
-        placeholder="Enter your name..."
-        value={nameInput}
-        autoFocus={true}
-        onChange={(e) => setNameInput(([e.target.name] = e.target.value))}
-      />
-      <button className="submitButton bg-blue-600 md:hover:scale-105">
-        Get In
-      </button>
-    </form>
+    <>
+      <form
+        className="flex justify-center gap-2"
+        onSubmit={(e) => {
+          e.preventDefault();
+          if (nameInput.length < 3)
+            return setStatus(
+              (prev) =>
+                (prev = {
+                  status: prev.status,
+                  message: "Name must be at least 3 characters",
+                })
+            );
+          getIn();
+        }}
+      >
+        <input
+          type="text"
+          name="name"
+          className="inputElement"
+          placeholder="Enter your name..."
+          value={nameInput}
+          autoFocus={true}
+          onChange={(e) => setNameInput(([e.target.name] = e.target.value))}
+        />
+        <button className="submitButton bg-blue-600 md:hover:scale-105">
+          Get In
+        </button>
+      </form>
+    </>
   );
 };
