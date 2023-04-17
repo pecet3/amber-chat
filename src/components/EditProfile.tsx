f0663541a19cfa451f328d24a4cddae109d9162f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-17 13:11:43 +0200

    created the component

---
+++ src/components/EditProfile.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { updateProfile, User } from "firebase/auth";
+import { auth } from "../firebase-config";
+export const EditProfile = () => {
+  const [name, setName] = React.useState({
+    name: "",
+  });
+  const updateDisplayName = async () => {
+    try {
+      updateProfile(auth.currentUser as User, {
+        displayName: "test1234",
+      });
+    } catch (err) {
+      alert(err);
+    }
+  };
+  return (
+    <section className="flex">
+      <form>
+        <input type="text" className="inputElement" name="name" value={name} />
+      </form>
+    </section>
+  );
+};
