61dcc2eb8455cf6325fcb9386e506875fee45759
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-18 00:06:42 +0200

    implemented status, created button

--- src/components/Auth/index.tsx
+++ src/components/Auth/index.tsx
@@ -10,12 +10,21 @@ import { Anonymous } from "./Anonymous";
 import Context, { TContext } from "../../ChatContext";
 
 export const Auth: React.FC = () => {
-  const { user, setUser } = React.useContext(Context) as TContext;
+  const { user, setUser, status, setStatus } = React.useContext(
+    Context
+  ) as TContext;
   const [click, setClick] = React.useState(false);
 
   React.useEffect(() => {
     const unSubscribe = onAuthStateChanged(auth, (currentUser) => {
       setUser(currentUser);
+      setStatus(
+        (prev) =>
+          (prev = {
+            status: "success",
+            message: "",
+          })
+      );
     });
 
     return () => unSubscribe();
@@ -23,41 +32,52 @@ export const Auth: React.FC = () => {
   return (
     <>
       <Header />
-      <motion.div
-        animate={{ x: 0, y: 0, scale: 1 }}
-        initial={{ x: -300, y: 0, scale: 0 }}
-        transition={{ duration: 0.7 }}
-      >
-        <p className="my-6">Log in as Guest</p>
-        <Anonymous />
-      </motion.div>
-
-      <motion.div
-        animate={{ x: 0, y: 0, scale: 1 }}
-        initial={{ x: 300, y: 0, scale: 0 }}
-        transition={{ duration: 0.5 }}
-        drag={true}
-      >
-        <p className="my-6">or...</p>
-        {click ? <LogIn /> : <Register />}
-      </motion.div>
-      <div className="my-6 flex justify-center gap-2">
+      <main>
         <motion.div
           animate={{ x: 0, y: 0, scale: 1 }}
           initial={{ x: -300, y: 0, scale: 0 }}
-          transition={{ duration: 0.5 }}
+          transition={{ duration: 0.7 }}
         >
-          <GoogleAuth />
+          <p className="my-6">Log in as Guest</p>
+          <Anonymous />
         </motion.div>
 
         <motion.div
           animate={{ x: 0, y: 0, scale: 1 }}
           initial={{ x: 300, y: 0, scale: 0 }}
-          transition={{ duration: 0.6 }}
+          transition={{ duration: 0.5 }}
+          drag={true}
         >
-          <GoogleAuth />
+          {status.message ? (
+            <p className="my-6 text-lg text-red-900">{status.message}</p>
+          ) : (
+            <p className="my-6">or...</p>
+          )}
+          {click ? <LogIn /> : <Register />}
         </motion.div>
-      </div>
+        <div className="my-6 flex justify-center gap-2">
+          <motion.div
+            animate={{ x: 0, y: 0, scale: 1 }}
+            initial={{ x: -300, y: 0, scale: 0 }}
+            transition={{ duration: 0.5 }}
+          >
+            <button
+              onClick={() => setClick((prev) => (prev = !prev))}
+              className="submitButton"
+            >
+              I {!click ? "have" : "Don't have"} an account
+            </button>
+          </motion.div>
+
+          <motion.div
+            animate={{ x: 0, y: 0, scale: 1 }}
+            initial={{ x: 300, y: 0, scale: 0 }}
+            transition={{ duration: 0.6 }}
+          >
+            <GoogleAuth />
+          </motion.div>
+        </div>
+      </main>
     </>
   );
 };
