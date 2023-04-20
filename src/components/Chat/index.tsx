5d226c73c12cf1fb5ffb8c9e12a1b3560d016500
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-20 19:01:01 +0200

    removed imports

--- src/components/Chat/index.tsx
+++ src/components/Chat/index.tsx
@@ -2,7 +2,8 @@ import { useRef } from "react";
 import { ChatHeader } from "./ChatHeader";
 import { Form } from "./Form";
 import { Messages } from "./Messages";
-
+import { IoIosAirplane } from "react-icons/io";
+import { motion } from "framer-motion";
 export interface IChat {}
 export const Chat: React.FC<IChat> = () => {
   const scrollRef = useRef<HTMLDivElement | null>(null);
@@ -12,6 +13,7 @@ export const Chat: React.FC<IChat> = () => {
       <ChatHeader scrollRef={scrollRef} />
       <main className=" bg-slate-200 dark:bg-slate-800">
         <Messages scrollRef={scrollRef} />
+
         <Form />
       </main>
     </>
