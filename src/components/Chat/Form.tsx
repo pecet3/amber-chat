e1791f3c97337c5b3edcb9ed44049fa1d88cd9f1
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-21 18:58:18 +0200

    uploaded tailwind

--- src/components/Chat/Form.tsx
+++ src/components/Chat/Form.tsx
@@ -62,31 +62,35 @@ export const Form: React.FC<IForm> = () => {
     });
   };
   return (
-    <form
-      noValidate
-      onSubmit={handleSubmit}
-      className="sticky z-10 m-auto flex
+    <>
+      <div className="m-auto flex max-w-3xl items-center justify-around rounded-t-lg bg-stone-300 ">
+        <input type="file" onChange={handleUploadChange} />
+        <button onClick={uploadImage} className="submitButton">
+          Upload
+        </button>
+      </div>
+      <form
+        noValidate
+        onSubmit={handleSubmit}
+        className="sticky z-10 m-auto flex
           max-w-[780px] gap-2 rounded-md bg-stone-300 p-1 shadow-xl md:p-2"
-    >
-      <input
-        placeholder="Type your message..."
-        onChange={handleInputChange}
-        value={newMessage}
-        autoFocus={true}
-        ref={inputRef}
-        className="max-w-3xl basis-8/12 rounded-md border-2 p-3"
-      />
-      <button
-        type="submit"
-        name="submitButton"
-        className="submitButton m-0 basis-4/12 bg-purple-600 px-4"
       >
-        Send
-      </button>
-      <input type="file" onChange={handleUploadChange} />
-      <button onClick={uploadImage} className="submitButton">
-        upload
-      </button>
-    </form>
+        <input
+          placeholder="Type your message..."
+          onChange={handleInputChange}
+          value={newMessage}
+          autoFocus={true}
+          ref={inputRef}
+          className="max-w-3xl basis-9/12 rounded-md border-2 p-3"
+        />
+        <button
+          type="submit"
+          name="submitButton"
+          className="submitButton m-0 basis-3/12 bg-purple-600 px-0 md:px-2 lg:px-4"
+        >
+          Send
+        </button>
+      </form>
+    </>
   );
 };
