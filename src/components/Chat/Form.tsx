85f8690d230336766813f44723cb377caed4671f
Jakub Pacewicz <pecet3107@gmail.coim>
2023-04-24 19:56:55 +0200

    removed functions of upload img

--- src/components/Chat/Form.tsx
+++ src/components/Chat/Form.tsx
@@ -1,21 +1,15 @@
 import { useState, useRef, useContext, useEffect } from "react";
 import Context, { TContext } from "../../ChatContext";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
-import { ref, uploadBytes, listAll, getDownloadURL } from "firebase/storage";
 import { auth, db, storage } from "../../firebase-config";
-import { nanoid } from "nanoid";
 export interface IForm {}
 export const Form: React.FC<IForm> = () => {
   const [newMessage, setNewMessage] = useState("");
-  const [upload, setUpload] = useState<File | null>(null);
-  const [imgListURL, setImgListURL] = useState([""]);
 
   const { anonymousUser, room } = useContext(Context) as TContext;
   const inputRef = useRef<HTMLInputElement | null>(null);
   const scrollRef = useRef<HTMLDivElement | null>(null);
   const messagesRef = collection(db, "messages");
-
-  const imageListRef = ref(storage, `images/${room}`);
 
   const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
@@ -52,41 +46,8 @@ export const Form: React.FC<IForm> = () => {
     scrollRef.current?.scrollIntoView({ behavior: "smooth" });
   };
 
-  const handleUploadChange = (event: React.FormEvent): void => {
-    const inputElement = event.target as HTMLInputElement;
-    if (inputElement && inputElement.files) {
-      setUpload(inputElement.files[0]);
-    }
-  };
-  const uploadImage = () => {
-    if (upload == null) return;
-    const imageRef = ref(storage, `images/${room}/${upload.name + nanoid()}`);
-    uploadBytes(imageRef, upload).then(() => {
-      alert("successfully uploaded");
-    });
-  };
-
-  useEffect(() => {
-    listAll(imageListRef).then((response) => {
-      response.items.forEach((item) => {
-        getDownloadURL(item).then((url) => {
-          setImgListURL((prev) => [...prev, url]);
-        });
-      });
-    });
-  }, []);
-
-  useEffect(() => {
-    console.log(imgListURL);
-  }, [imgListURL]);
   return (
     <>
-      {/* <div className="m-auto flex max-w-3xl items-center justify-around rounded-t-lg bg-stone-300 ">
-        <input type="file" onChange={handleUploadChange} />
-        <button onClick={uploadImage} className="submitButton">
-          Upload
-        </button>
-      </div> */}
       <form
         noValidate
         onSubmit={handleSubmit}
